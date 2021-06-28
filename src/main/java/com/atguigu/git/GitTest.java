4def51b53129fd824088a3db7bf71bba553dbb60
hxjstorm <storm@hdu.edu.cn>
2021-06-29 00:11:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author hxj
+ * @create 2021-06-29 0:07
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
